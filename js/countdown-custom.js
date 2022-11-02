dcca84a5f76e8d420d89884b8270b4e4d0ace020
mipapamedijo <crocolite96@gmail.com>
2022-11-01 19:11:10 -0600

    debut

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2023, 11, 03, 12)}); // year, month, date, hour
+        });
+});		
+
